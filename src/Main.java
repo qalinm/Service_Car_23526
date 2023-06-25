6e420b11e475a5f29ad2458f2ab325111553f9fa
qalinm <kalinskipa@interia.pl>
2023-06-25 11:59:34 +0200

    usunięcie niepotrzebnego fragmentu

--- src/Main.java
+++ src/Main.java
@@ -15,9 +15,6 @@ public class Main {
         String filterCompany = scanner.nextLine();
         scanner.close();
 
-        List<Car> filteredCars = filterCarsByCompany(cars, filterCompany);
-        System.out.println("\nLista aut po filtrowaniu dla firmy " + filterCompany + ":");
-        displayCarListWithNumbering(filteredCars);
     }
 
 
